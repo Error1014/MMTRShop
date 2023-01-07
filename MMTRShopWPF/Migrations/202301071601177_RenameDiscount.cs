99dae3515dc5e8549468790c2ea024c9c85a4ac8
the Error1024  was busy <kourov1024@gmail.com>
2023-01-07 19:11:07 +0300

    мини исправления

---
+++ MMTRShopWPF/Migrations/202301071601177_RenameDiscount.cs
@@ -0,0 +1,20 @@
+﻿namespace MMTRShopWPF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameDiscount : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "Discount", c => c.Single(nullable: false));
+            DropColumn("dbo.Products", "Diccount");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Products", "Diccount", c => c.Single(nullable: false));
+            DropColumn("dbo.Products", "Discount");
+        }
+    }
+}
