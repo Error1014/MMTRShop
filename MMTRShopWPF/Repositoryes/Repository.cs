29da7a714640271c94db9af914f5281496ebce39
the Error1024  was busy <kourov1024@gmail.com>
2023-01-08 21:02:06 +0300

    Добавлен репозиторий

---
+++ MMTRShopWPF/Repositoryes/Repository.cs
@@ -0,0 +1,55 @@
+﻿using MMTRShopWPF.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMTRShopWPF.Repositoryes
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+{
+        protected readonly ShopContext Context;
+
+        public Repository(ShopContext context)
+        {
+            Context = context;
+        }
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
