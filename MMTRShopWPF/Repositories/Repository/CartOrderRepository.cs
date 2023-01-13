a7ecb152711053ab4d39d3b23597f29db18c8302
the Error1024  was busy <kourov1024@gmail.com>
2023-01-13 14:32:03 +0300

    Созданы Repositories для работы с заказами

---
+++ MMTRShopWPF/Repositories/Repository/CartOrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using MMTRShopWPF.Model;
+using MMTRShopWPF.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMTRShopWPF.Repositories.Repository
+{
+    public class CartOrderRepository:Repository<CartOrder>, ICartOrderRepository
+    {
+        public CartOrderRepository(ShopContext context) : base(context)
+        {
+
+        }
+
+        public ShopContext ShopContext
+        {
+            get { return Context as ShopContext; }
+        }
+    }
+}
