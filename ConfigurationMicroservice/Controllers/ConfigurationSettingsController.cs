665f10854dd71d2cc0316a9eb283ae4d866740e4
the Error1024  was busy <kourov1024@gmail.com>
2023-03-16 11:52:41 +0300

    Доработка контроллеров Configuration

--- ConfigurationMicroservice/Controllers/ConfigurationSettingsController.cs
+++ ConfigurationMicroservice/Controllers/ConfigurationSettingsController.cs
@@ -1,10 +1,10 @@
 ﻿using ConfigurationMicroservice.Configuration.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Infrastructure.DTO;
 
 namespace ConfigurationMicroservice.Configuration.Api.Controllers
 {
-    [Authorize(Roles = "Admin")]
     public class ConfigurationSettingsController : BaseApiController
     {
         private readonly IClientSettingsService _configurationService;
@@ -12,12 +12,19 @@ namespace ConfigurationMicroservice.Configuration.Api.Controllers
         {
             _configurationService = configurationService;
         }
+        [HttpGet(nameof(GetConfiguration))]
+        public async Task<ActionResult<SettingsAPI>> GetConfiguration()
+        {
+            return Ok(_configurationService.SettingsAPI);
+        }
+        [Authorize(Roles = "Admin")]
         [HttpPut(nameof(PutRestrictionOfGoodsInCart))]
         public async Task<IActionResult> PutRestrictionOfGoodsInCart(int value)
         {
             _configurationService.UpdateRestrictionOfGoodsInCart(value);
             return Ok();
         }
+        [Authorize(Roles = "Admin")]
         [HttpPut(nameof(PutQuantityOfProductToDisplay))]
         public async Task<IActionResult> PutQuantityOfProductToDisplay(int value)
         {
