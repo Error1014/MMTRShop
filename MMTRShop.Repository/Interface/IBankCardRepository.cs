3ab1913e9a4b77be905ebda49a8f5cc3d0099280
the Error1024  was busy <kourov1024@gmail.com>
2023-03-29 15:46:53 +0300

    Чистка проекта

--- MMTRShop.Repository/Interface/IBankCardRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MMTRShop.Repository.Entities;
-using System;
-
-namespace MMTRShop.Repository.Interface
-{
-    public interface IBankCardRepository: IRepository<BankCard,Guid>
-    {
-    }
-}
